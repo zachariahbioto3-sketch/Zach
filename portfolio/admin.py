324dcef068efa40331cec7863c42320d72a98af9
zachariahbioto3-sketch <zachariahbioto3@gmail.com>
2026-06-26 08:34:23 +0000

    new template were added

--- portfolio/admin.py
+++ portfolio/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import WebsiteTemplate
 
-# Register your models here.
+@admin.register(WebsiteTemplate)
+class WebsiteTemplateAdmin(admin.ModelAdmin):
+    list_display  = ('title', 'category', 'price', 'is_featured', 'created_at')
+    list_filter   = ('category', 'is_featured')
+    search_fields = ('title', 'description')
+    prepopulated_fields = {'slug': ('title',)}
